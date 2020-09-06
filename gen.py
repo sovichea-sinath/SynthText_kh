19013fb32012e22398cb0fa696906c2d8f05194a
sovichea-sinath <sinathsovichea@gmail.com>
2020-09-06 21:35:26 +0700

    init

--- gen.py
+++ gen.py
@@ -30,7 +30,7 @@ SECS_PER_IMG = 5 #max time per image in seconds
 DATA_PATH = 'data'
 DB_FNAME = osp.join(DATA_PATH,'dset.h5')
 # url of the data (google-drive public file):
-DATA_URL = 'http://www.robots.ox.ac.uk/~ankush/data.tar.gz'
+DATA_URL = 'http://wwdw.robots.ox.ac.uk/~ankush/data.tar.gz'
 OUT_FILE = 'results/SynthText.h5'
 
 def get_data():
